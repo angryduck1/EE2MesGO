e4ce8e19536d9124c16b4b22a58ecd2dbce5c132
angryduck001 <98224800+angryduck1@users.noreply.github.com>
2026-06-23 14:00:17 +0300

    Update crypto.go

--- server/crypto.go
+++ server/crypto.go
@@ -35,12 +35,3 @@ func generateDeviceToken() (string, error) {
 
 	return token.String(), nil
 }
-
-func generateDeviceToken() (string, error) {
-	token, err := uuid.NewRandom()
-	if err != nil {
-		return "", fmt.Errorf("failed to generate device token: %v", err)
-	}
-
-	return token.String(), nil
-}
